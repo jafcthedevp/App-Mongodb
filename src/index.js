fab57ca162360a398586c85aec1f6ef80b7c1cfd
jafcthedevp <1275435@senati.pe>
2024-03-10 14:56:56 -0500

    segundo commit

--- src/index.js
+++ src/index.js
@@ -1,26 +1,38 @@
 const express = require('express');
 const path = require('path');
-const exphbs = require('express-handlebars');
+const { engine } = require('express-handlebars');
+const methodOverride = require('method-override');
+const session = require('express-session');
+
 // Initialize
 const app = express();
 
 //Setings 
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-app.engine('.hbs', exphbs({
+app.engine('.hbs', engine({
     defaultLayout: 'main',
     layoutsDir: path.join(app.get('views'), 'layouts'),
     partialsDir: path.join(app.get('views'), 'partials'),
     extname: '.hbs'
 }));
 
-app.set('views engine', 'hbs');
+app.set('view engine', 'hbs');
 
 //Midlewares 
 app.use(express.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
+app.use(session({
+    secret: 'myscretapp',
+    resave: true,
+    saveUninitialized: true
+}));
 //Global Variables
 
 //Routes
+app.use(require('./routes/index'));
+app.use(require('./routes/notes'));
+app.use(require('./routes/users'));
 
 // Static Files
 
