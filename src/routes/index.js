255f2a98c524380df67520ef7d5f8f5597654f3c
jafcthedevp <1275435@senati.pe>
2024-03-14 13:26:02 -0500

    cuarto commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Index');
+    res.render('index');
 });
 
 router.get('/about', (req, res) => {
-    res.send('About');
+    res.render('about');
 });
 
 module.exports = router;
--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/notes/add', (req, res) => {
+    res.render('notes/new-notes');
+});
+
+router.post('/notes/new-note', (req, res) => {
+    const { title, description }= req.body;
+    res.send('ok');
+});
+
 router.get('/notes', (req, res) => {
     res.send('Notes from database');
 });
